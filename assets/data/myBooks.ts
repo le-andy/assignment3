14c1345d993ff96c11c668e5e197f25885e92411
le-andy <52579182+le-andy@users.noreply.github.com>
2019-07-26 16:51:37 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,8 @@
+import {BookInfo} from '../../app/BookInfo';
+
+export const MYBOOKS: BookInfo[] = [
+    {b_name: 'The Ethics of Aristotle', b_author: 'Aristotle', b_genre: 'Non-fiction', b_year_published: '1976', b_picture: '../../assets/images/ethics.jpg'},
+    {b_name: 'Object-Oriented Analysis and Design: With the Unified Process', b_author: 'John W. Satzinger', b_genre: 'Reference', b_year_published: '2004', b_picture: '../../assets/images/uml.jpg'},
+    {b_name: 'CCNA Cisco Certified Network Associate Study Guide, 7th Edition', b_author: 'Todd Lammle', b_genre: 'Reference', b_year_published: '2011', b_picture: '../../assets/images/ccna.jpg'},
+    {b_name: 'Canadian Microeconomics: Problems & Policies, 11th Edition', b_author: 'Brian Lyons', b_genre: 'Reference', b_year_published: '2015', b_picture: '../../assets/images/macroeconomics.jpg'}
+]
